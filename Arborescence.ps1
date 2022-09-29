91548c6caabcc50a5dc54f3f9206cd08b456ac3a
TheoFA24 <tfadier@myges.fr>
2022-09-29 15:46:59 +0200

    feat: Added Arborescence script

---
+++ Arborescence.ps1
@@ -0,0 +1,10 @@
+﻿$dossier = Read-Host "Chemin du dossier souhaité"
+$enfant = Get-ChildItem $test
+
+foreach ($en in $enfant)
+{
+    if ($en.Mode -like "*a*")
+    {
+        Write-Host "Voici un des fichiers avec les droits lié à celui-ci $($en) $($en.Mode) et leur taille $($en.Length) dans le dossier $dossier"
+    }
+}
