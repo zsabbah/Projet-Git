b3fd9aff563e5039a26cbba672bc8bb0ad1758f9
TheoFA24 <tfadier@myges.fr>
2022-09-29 15:02:48 +0200

    feat: Added count.py script

---
+++ count.py
@@ -0,0 +1,9 @@
+min = int(1)
+max = int(100)
+for n in range(min,max + 1):
+   if n > 1:
+       for i in range(2,n):
+           if (n % i) == 0:
+               break
+       else:
+           print(n)
